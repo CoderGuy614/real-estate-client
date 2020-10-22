5ee0c311a920b750cb80e4e4b6790cd8931440c7
CoderGuy614 <jlutz.110@gmail.com>
2020-10-22 07:45:37 -0400

    Eliminate Console Error

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -21,10 +21,10 @@ const Nav = () => {
         <Navbar.Collapse className="justify-content-end align-baseline">
           <BootstrapNav>
             <div className="d-none d-md-block social-container">
-              <i class="fab fa-facebook-square fa-2x social-icon"></i>
-              <i class="fab fa-instagram-square fa-2x social-icon"></i>
-              <i class="fab fa-twitter-square fa-2x social-icon"></i>
-              <i class="fab fa-whatsapp-square fa-2x social-icon"></i>
+              <i className="fab fa-facebook-square fa-2x social-icon"></i>
+              <i className="fab fa-instagram-square fa-2x social-icon"></i>
+              <i className="fab fa-twitter-square fa-2x social-icon"></i>
+              <i className="fab fa-whatsapp-square fa-2x social-icon"></i>
             </div>
           </BootstrapNav>
         </Navbar.Collapse>
